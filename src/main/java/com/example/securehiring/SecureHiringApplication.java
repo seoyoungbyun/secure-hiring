5f56f55faca94de1e634aed7606a92a843123797
seoyoungbyun <qustjdud2181@gmail.com>
2025-05-23 08:54:01 +0900

    [init]: 프로젝트 세팅

---
+++ src/main/java/com/example/securehiring/SecureHiringApplication.java
@@ -0,0 +1,13 @@
+package com.example.securehiring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureHiringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureHiringApplication.class, args);
+	}
+
+}
